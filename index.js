1510bbdfb9856b4521b9d9784406aef2fee2cf45
Clint <138284298+cl1ntdev@users.noreply.github.com>
2024-07-11 00:23:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,109 @@
-function _0x5eb7(_0x3b7fab,_0x58c545){const _0x585bdb=_0x2623();return _0x5eb7=function(_0xba90cf,_0x2b98b7){_0xba90cf=_0xba90cf-(-0x49a+-0x11a7*0x1+-0x1*-0x174a);let _0x2eb77c=_0x585bdb[_0xba90cf];return _0x2eb77c;},_0x5eb7(_0x3b7fab,_0x58c545);}const _0x20c8f8=_0x5eb7;(function(_0x10752c,_0x201211){const _0x5c8b0d=_0x5eb7,_0x93daf0=_0x10752c();while(!![]){try{const _0x567b0b=-parseInt(_0x5c8b0d(0x111))/(0x573+0x2333*0x1+-0x28a5)*(parseInt(_0x5c8b0d(0x142))/(0xa*0x277+-0x741*0x3+-0x2e1))+parseInt(_0x5c8b0d(0x133))/(0x1*-0x1087+-0x22aa+0x3334)+-parseInt(_0x5c8b0d(0x116))/(0x6fa+0xf63+0x3*-0x773)*(parseInt(_0x5c8b0d(0x1a1))/(0xcbd*-0x1+-0x9e*0x1+-0x10*-0xd6))+-parseInt(_0x5c8b0d(0x146))/(0x1a*-0x11+-0x207b*-0x1+0x1*-0x1ebb)*(-parseInt(_0x5c8b0d(0x119))/(-0x7*-0x2c3+0x93c+-0x1c8a))+parseInt(_0x5c8b0d(0x16a))/(-0x174c+0x44+0xc*0x1ec)*(-parseInt(_0x5c8b0d(0x137))/(0x71+-0x4*-0x6f5+-0x2*0xe1e))+-parseInt(_0x5c8b0d(0x17f))/(-0x24e+-0x1e0b+0x2063*0x1)*(parseInt(_0x5c8b0d(0x11a))/(-0x25e5+0xa32+0x1bbe))+parseInt(_0x5c8b0d(0x184))/(-0x25b+0x295*-0x7+-0x1*-0x147a);if(_0x567b0b===_0x201211)break;else _0x93daf0['push'](_0x93daf0['shift']());}catch(_0x1ae454){_0x93daf0['push'](_0x93daf0['shift']());}}}(_0x2623,0x68be9+0x26237+-0x56afc));import{initializeApp}from'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';import{getDatabase,ref,push,onValue,remove,update}from'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';const appSettings={'databaseURL':_0x20c8f8(0x150)+_0x20c8f8(0x192)+_0x20c8f8(0x112)+_0x20c8f8(0x161)+_0x20c8f8(0x14c)+_0x20c8f8(0x118)},app=initializeApp(appSettings),db=getDatabase(app),response=ref(db,_0x20c8f8(0x12a));document[_0x20c8f8(0x168)+_0x20c8f8(0x159)](_0x20c8f8(0x17c)+'z')[_0x20c8f8(0x188)+_0x20c8f8(0x122)](_0x20c8f8(0x110),_0x2b26c8=>{const _0x3851db=_0x20c8f8,_0x313ee3={'UxnHU':function(_0x123403,_0x129106){return _0x123403(_0x129106);}};_0x313ee3[_0x3851db(0x10e)](startRizz,_0x2b26c8[_0x3851db(0x180)+_0x3851db(0x1a0)]);});function startRizz(_0x16000b){const _0x1e32ba=_0x20c8f8,_0x3d5be6={'anJLm':_0x1e32ba(0x154),'uLFhq':function(_0x515b85,_0x1432c){return _0x515b85(_0x1432c);},'TOVrp':_0x1e32ba(0x12c),'agzAU':_0x1e32ba(0x125)+'on','KbOEA':_0x1e32ba(0x138)+'er','skdUr':_0x1e32ba(0x115),'nTFwi':_0x1e32ba(0x172),'LtlUF':_0x1e32ba(0x16c),'rsUHA':_0x1e32ba(0x19f),'OZCRo':_0x1e32ba(0x16f),'uZuLF':_0x1e32ba(0x127),'EXtJF':_0x1e32ba(0x18a),'NCZDh':_0x1e32ba(0x1a9),'QDFFw':_0x1e32ba(0x19a)+_0x1e32ba(0x15c)+_0x1e32ba(0x166),'lznpE':_0x1e32ba(0x18e),'WRvvK':_0x1e32ba(0x181),'IjZmX':_0x1e32ba(0x158),'dvptR':_0x1e32ba(0x16d)};let _0x2aa029=document[_0x1e32ba(0x168)+_0x1e32ba(0x159)](_0x3d5be6[_0x1e32ba(0x109)])[_0x1e32ba(0x18d)]();_0x16000b[_0x1e32ba(0x1a2)]();let _0x32e440=document[_0x1e32ba(0x168)+_0x1e32ba(0x159)](_0x3d5be6[_0x1e32ba(0x121)]),_0x4e6151=document[_0x1e32ba(0x17a)+_0x1e32ba(0x196)](_0x3d5be6[_0x1e32ba(0x124)]);_0x4e6151[_0x1e32ba(0x10a)]=_0x3d5be6[_0x1e32ba(0x15f)],_0x4e6151[_0x1e32ba(0x175)]=_0x3d5be6[_0x1e32ba(0x173)],_0x4e6151[_0x1e32ba(0x113)][_0x1e32ba(0x12e)]=_0x3d5be6[_0x1e32ba(0x176)],_0x4e6151[_0x1e32ba(0x155)][_0x1e32ba(0x13e)](_0x3d5be6[_0x1e32ba(0x13b)]),_0x32e440[_0x1e32ba(0x11c)+'d'](_0x4e6151);let _0x17befd=document[_0x1e32ba(0x17a)+_0x1e32ba(0x196)](_0x3d5be6[_0x1e32ba(0x160)]);_0x17befd[_0x1e32ba(0x10a)]=_0x3d5be6[_0x1e32ba(0x123)],_0x17befd[_0x1e32ba(0x193)]=_0x3d5be6[_0x1e32ba(0x12b)],_0x17befd[_0x1e32ba(0x155)][_0x1e32ba(0x13e)](_0x3d5be6[_0x1e32ba(0x13b)]),_0x32e440[_0x1e32ba(0x11c)+'d'](_0x17befd);let _0x5abd5a=document[_0x1e32ba(0x17a)+_0x1e32ba(0x196)](_0x3d5be6[_0x1e32ba(0x160)]);_0x5abd5a[_0x1e32ba(0x10a)]=_0x3d5be6[_0x1e32ba(0x130)],_0x32e440[_0x1e32ba(0x11c)+'d'](_0x5abd5a);let _0x57c7cc=document[_0x1e32ba(0x17a)+_0x1e32ba(0x196)](_0x3d5be6[_0x1e32ba(0x19b)]);_0x57c7cc[_0x1e32ba(0x193)]=_0x3d5be6[_0x1e32ba(0x18c)],_0x57c7cc[_0x1e32ba(0x10a)]=_0x3d5be6[_0x1e32ba(0x18b)],_0x57c7cc[_0x1e32ba(0x155)][_0x1e32ba(0x13e)](_0x3d5be6[_0x1e32ba(0x13b)]),_0x57c7cc[_0x1e32ba(0x165)]=function(_0x540e33){const _0x4c8330=_0x1e32ba;document[_0x4c8330(0x168)+_0x4c8330(0x159)](_0x3d5be6[_0x4c8330(0x162)])[_0x4c8330(0x145)]=!![],_0x540e33[_0x4c8330(0x180)+_0x4c8330(0x1a0)][_0x4c8330(0x145)]=!![],_0x3d5be6[_0x4c8330(0x126)](action,_0x3d5be6[_0x4c8330(0x18b)]),document[_0x4c8330(0x16b)]=_0x3d5be6[_0x4c8330(0x179)];};let _0x2ff5c4=document[_0x1e32ba(0x17a)+_0x1e32ba(0x196)](_0x3d5be6[_0x1e32ba(0x19b)]);_0x2ff5c4[_0x1e32ba(0x165)]=function(_0x35da1e){const _0x5e6150=_0x1e32ba;_0x35da1e[_0x5e6150(0x180)+_0x5e6150(0x1a0)][_0x5e6150(0x145)]=!![],_0x57c7cc[_0x5e6150(0x145)]=!![],_0x3d5be6[_0x5e6150(0x126)](action,'no'),document[_0x5e6150(0x16b)]=_0x3d5be6[_0x5e6150(0x179)];},_0x2ff5c4[_0x1e32ba(0x193)]='No',_0x2ff5c4[_0x1e32ba(0x10a)]='no',_0x2ff5c4[_0x1e32ba(0x155)][_0x1e32ba(0x13e)](_0x3d5be6[_0x1e32ba(0x13b)]),_0x5abd5a[_0x1e32ba(0x11c)+'d'](_0x57c7cc),_0x5abd5a[_0x1e32ba(0x11c)+'d'](_0x2ff5c4);let _0x484219=document[_0x1e32ba(0x17a)+_0x1e32ba(0x196)]('h1');_0x484219[_0x1e32ba(0x10a)]=_0x3d5be6[_0x1e32ba(0x191)],_0x32e440[_0x1e32ba(0x11c)+'d'](_0x484219);}function _0x2623(){const _0x661490=['XcEoM','g\x20mga\x20oras','2cdfjCa','l\x20at\x20may\x20b','aguluhan','disabled','158754JxmyTZ','ta\x20na','Parehong\x20n','uin,\x20aking','timeupdate','agkaron\x20ng','rebaseio.c','hfTxh','Nagkasalub','Dahil\x20sa\x20i','https://sh','pit\x20at\x20nah','yong\x20ganda','sta\x20ka?','.no','classList','bi\x20na\x20kamu','Balahibo\x20t','Yes\x20<3','tor','Pinairal\x20a','text','o\x20out\x20with','uso\x20ko\x27y\x20t','jaDbj','LtlUF','EXtJF','lt-rtdb.fi','anJLm','ara\x20bang\x20h','log','onclick','\x20me?','getElement','querySelec','Kalungkuta','8CjMtLX','cookie','imager','lyr','Bahala\x20na','absolute','Lumipas\x20an','Pano\x20to?','img','rsUHA','floor','src','OZCRo','n\x20kong\x20bit','Hinahangaa','agzAU','createElem','libro\x20na\x20b','.start-riz','umatayo,\x20p','currentTim','135170bdJuiQ','currentTar','button','kot\x20ang\x20pa','ng\x20nadaram','9504504noNiFA',',\x20nabihag\x20','audioPlaye','alak','addEventLi','ap,\x20puso\x20p','div','TOVrp','IjZmX','play','ans','\x20dagat','\x20dulo','dvptR','eep-game-9','innerText','length','a\x20gilid\x20ng','ent','ById','IxhUC','Pasikot-si','Will\x20you\x20g','WRvvK','a\x27y\x20tumigi','Paningin\x20n','i\x20ay\x20nalan','./cat.jpg','get','5YsSPeo','remove','Unang\x20suly','Teka,\x20miss',',\x20ako\x27y\x20na','Ako\x27y\x20luma','lim','Tila\x27y\x20lum','que','KbOEA','className','n\x20sa\x20saril','d\x20kita','inabasa\x20ko','UxnHU','.lyr','click','158836iKVVph','8c81-defau','style','Sinundan\x20h','.container','1379628lVtlhA','agkatagpo','om/','7MXjtFC','297EEFrzW','sxuQv','appendChil','inihila\x20na','ng\x20ganda','apit,\x20naib','ZoIGE','skdUr','stener','NCZDh','nTFwi','rizzurecti','uLFhq','fade','ing\x20dalawa','time','response','QDFFw','yes','\x20na\x20ayaw\x20m','position','tulala','lznpE','\x20tinitinga','san\x20ang\x20di','1372320tpcfTH','iyang\x20sina','ong\x20tayo\x20s','ako\x20ng\x20iyo','1593531BvLPVn','#audioPlay','anggang\x20ik','Para\x20kang\x20','uZuLF','umitibok','ningin\x20nat','toggle','\x20at\x20hinati'];_0x2623=function(){return _0x661490;};return _0x2623();}var lyricsData=[{'time':0x0,'text':_0x20c8f8(0x1a4)+_0x20c8f8(0x185)+_0x20c8f8(0x136)+_0x20c8f8(0x11e)},{'time':0x3,'text':_0x20c8f8(0x1a3)+_0x20c8f8(0x189)+_0x20c8f8(0x163)+_0x20c8f8(0x11d)},{'time':0x6,'text':_0x20c8f8(0x14e)+_0x20c8f8(0x135)+_0x20c8f8(0x195)+_0x20c8f8(0x18f)},{'time':0x9,'text':_0x20c8f8(0x114)+_0x20c8f8(0x139)+_0x20c8f8(0x19c)+_0x20c8f8(0x143)+_0x20c8f8(0x187)},{'time':0xd,'text':_0x20c8f8(0x19d)+_0x20c8f8(0x117)},{'time':0x10,'text':_0x20c8f8(0x157)+_0x20c8f8(0x17d)+_0x20c8f8(0x15d)+_0x20c8f8(0x13c)},{'time':0x13,'text':_0x20c8f8(0x13a)+_0x20c8f8(0x17b)+_0x20c8f8(0x10d)+_0x20c8f8(0x12d)+_0x20c8f8(0x14b)+_0x20c8f8(0x190)},{'time':0x18,'text':_0x20c8f8(0x171)},{'time':0x19,'text':_0x20c8f8(0x148)+_0x20c8f8(0x144)},{'time':0x1c,'text':_0x20c8f8(0x199)+_0x20c8f8(0x182)+_0x20c8f8(0x13d)+_0x20c8f8(0x128)},{'time':0x20,'text':_0x20c8f8(0x16e)},{'time':0x22,'text':_0x20c8f8(0x1a6)+_0x20c8f8(0x151)+_0x20c8f8(0x134)+_0x20c8f8(0x156)+_0x20c8f8(0x153)},{'time':0x26,'text':_0x20c8f8(0x15a)+_0x20c8f8(0x183)+'a'},{'time':0x28,'text':_0x20c8f8(0x169)+_0x20c8f8(0x10b)+_0x20c8f8(0x19e)+_0x20c8f8(0x147)},{'time':0x2b,'text':_0x20c8f8(0x14f)+_0x20c8f8(0x152)+_0x20c8f8(0x1a5)+_0x20c8f8(0x12f)},{'time':0x2e,'text':_0x20c8f8(0x170)+_0x20c8f8(0x141)+_0x20c8f8(0x13f)+_0x20c8f8(0x10c)},{'time':0x31,'text':_0x20c8f8(0x178)+_0x20c8f8(0x177)+_0x20c8f8(0x149)+_0x20c8f8(0x131)+'la'},{'time':0x38,'text':_0x20c8f8(0x1a8)+_0x20c8f8(0x11f)+_0x20c8f8(0x132)+_0x20c8f8(0x1a7)}];document[_0x20c8f8(0x167)+_0x20c8f8(0x197)](_0x20c8f8(0x186)+'r')[_0x20c8f8(0x188)+_0x20c8f8(0x122)](_0x20c8f8(0x14a),function(){const _0x2636a5=_0x20c8f8,_0x66d2bb={'IxhUC':_0x2636a5(0x10f),'jaDbj':function(_0x1ec002,_0x4d6d09){return _0x1ec002<_0x4d6d09;},'hfTxh':function(_0x267379,_0x1175d9){return _0x267379===_0x1175d9;},'XcEoM':function(_0x3af6c2,_0xd83252){return _0x3af6c2(_0xd83252);}};var _0xb28cdc=Math[_0x2636a5(0x174)](this[_0x2636a5(0x17e)+'e']);console[_0x2636a5(0x164)](_0xb28cdc);let _0x2128b6=document[_0x2636a5(0x168)+_0x2636a5(0x159)](_0x66d2bb[_0x2636a5(0x198)]);for(var _0x113121=-0x8*-0x70+0x1ce0+-0x1c*0x128;_0x66d2bb[_0x2636a5(0x15e)](_0x113121,lyricsData[_0x2636a5(0x194)]);_0x113121++){_0x66d2bb[_0x2636a5(0x14d)](_0x66d2bb[_0x2636a5(0x140)](parseInt,lyricsData[_0x113121][_0x2636a5(0x129)]),_0xb28cdc)&&(_0x2128b6[_0x2636a5(0x193)]=lyricsData[_0x113121][_0x2636a5(0x15b)]);}});function action(_0x23bf23){const _0x24dc24=_0x20c8f8,_0x5abd16={'sxuQv':_0x24dc24(0x12c),'ZoIGE':function(_0x38d0cb,_0x4d3e6d,_0x1543b1){return _0x38d0cb(_0x4d3e6d,_0x1543b1);}};switch(_0x23bf23){case _0x5abd16[_0x24dc24(0x11b)]:_0x5abd16[_0x24dc24(0x120)](push,response,_0x5abd16[_0x24dc24(0x11b)]);break;case'no':_0x5abd16[_0x24dc24(0x120)](push,response,'no');break;}}
+        
+        // console.log('connected')
+        import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+        import { getDatabase, ref, push, onValue, remove, update} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+
+        const appSettings = {
+            databaseURL:'https://sheep-game-98c81-default-rtdb.firebaseio.com/'
+        }
+
+        const app = initializeApp(appSettings)//gets the app
+        const db = getDatabase(app)//gets the database in app
+        const response = ref(db,"response")
+
+
+        document.querySelector('.start-rizz').addEventListener('click',(e)=>{
+            startRizz(e.currentTarget)
+        })
+
+        function startRizz(obj){
+            let audio = document.querySelector('#audioPlayer').play()
+            obj.remove()
+            let cont = document.querySelector('.container')
+            let newImg = document.createElement('img')
+            newImg.className = 'imager'
+            newImg.src = './cat.jpg'
+            newImg.style.position = 'absolute'
+            newImg.classList.toggle('fade')
+            cont.appendChild(newImg)
+            let newQuestion = document.createElement('div')
+            newQuestion.className = 'que'
+            newQuestion.innerText = 'Will you go out with me?'
+            newQuestion.classList.toggle('fade')
+            cont.appendChild(newQuestion)
+            let ansShow = document.createElement('div')
+            ansShow.className = 'ans'
+            cont.appendChild(ansShow)
+            let yes = document.createElement('button')
+            yes.innerText = 'Yes <3'
+            yes.className = 'yes'
+            yes.classList.toggle('fade')
+            yes.onclick = function(e){
+                document.querySelector('.no').disabled = true
+                e.currentTarget.disabled = true
+                action('yes')
+                document.cookie = 'rizzurection'
+            }
+            let No = document.createElement('button')
+            No.onclick = function(e){
+                e.currentTarget.disabled = true
+                yes.disabled = true
+                action('no')
+                document.cookie = 'rizzurection'
+            }
+            
+            No.innerText = 'No'
+            No.className = 'no'
+            No.classList.toggle('fade')
+            ansShow.appendChild(yes) 
+            ansShow.appendChild(No) 
+            let lyrics = document.createElement('h1')
+            lyrics.className = 'lyr'
+            // lyrics.zIndex = '1'
+            cont.appendChild(lyrics)
+        }
+        var lyricsData = [
+        { time: 0, text: "Teka, miss, nabihag ako ng iyong ganda" },
+        { time: 3, text: "Unang sulyap, puso para bang hinihila na" },
+        { time: 6, text: "Nagkasalubong tayo sa gilid ng dagat" },
+        { time: 9, text: "Sinundan hanggang ika'y tumigil at may balak" },
+        { time: 13, text: "Paningin nagkatagpo" },
+        { time: 16, text: "Balahibo tumatayo, puso ko'y tumitibok" },
+        { time: 19, text: "Para kang libro na binabasa ko na ayaw magkaron ng dulo" },
+        { time: 24, text: "Pano to?" },
+        { time: 25, text: "Parehong naguluhan" },
+        { time: 28, text: "Pasikot-sikot ang paningin nating dalawa" },
+        { time: 32, text: "Bahala na" },
+        { time: 34, text: "Ako'y lumapit at nahiyang sinabi na kamusta ka?" },
+        { time: 38, text: "Pinairal ang nadarama" },
+        { time: 40, text: "Kalungkutan sa sarili ay nalanta na"},
+        { time: 43, text: "Dahil sa iyong ganda, ako'y natulala"},
+        { time: 46, text: "Lumipas ang mga oras at hinatid kita"},
+        { time: 49, text: "Hinahangaan kong bituin, aking tinitingala"},
+        { time: 56, text: "Tila'y lumapit, naibsan ang dilim"},
+        ];
+        document.getElementById('audioPlayer').addEventListener('timeupdate', function() {
+        var currentTime = Math.floor(this.currentTime);
+        // console.log(currentTime)
+        let lyr = document.querySelector('.lyr')
+            for (var i = 0; i < lyricsData.length; i++) {
+            if(parseInt(lyricsData[i].time) === currentTime){
+                lyr.innerText = lyricsData[i].text
+            }
+            }
+        });
+
+
+        function action(val){
+            switch(val){
+                case 'yes': 
+                push(response,"yes")
+                    break;
+                case 'no': 
+                push(response,"no")
+
+                    break;
+            }
+        }
+
+       
